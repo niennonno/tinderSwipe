7b0a48d4f1dbafe12a3f9540124ba90ced101aab
Aditya Vikram Godawat <aditya.godawat@wowlabz.com>
2016-05-16 14:23:07 +0530

    first commit

---
+++ tinderSwipe/OverlayView.swift
@@ -0,0 +1,49 @@
+//
+//  OverlayView.swift
+//  tinderSwipe
+//
+//  Created by Aditya Vikram Godawat on 16/05/16.
+//  Copyright © 2016 Wow Labz. All rights reserved.
+//
+
+import UIKit
+
+
+enum GGOverlayViewMode {
+    case GGOverlayViewModeLeft
+    case GGOverlayViewModeRight
+}
+
+class OverlayView: UIView{
+    var _mode: GGOverlayViewMode! = GGOverlayViewMode.GGOverlayViewModeLeft
+    var imageView: UIImageView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.whiteColor()
+       // imageView = UIImageView(image: UIImage(named: "noButton"))
+      //  self.addSubview(imageView)
+    }
+    
+    func setMode(mode: GGOverlayViewMode) -> Void {
+        if _mode == mode {
+            return
+        }
+        _mode = mode
+        
+        if _mode == GGOverlayViewMode.GGOverlayViewModeLeft {
+         //   imageView.image = UIImage(named: "noButton")
+        } else {
+        //    imageView.image = UIImage(named: "yesButton")
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+  //      imageView.frame = CGRectMake(50, 50, 100, 100)
+    }
+}
---
+++ tinderSwipe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tinderSwipe
+//
+//  Created by Aditya Vikram Godawat on 16/05/16.
+//  Copyright © 2016 Wow Labz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let draggableBackground: DraggableViewBackground = DraggableViewBackground(frame: self.view.frame)
+        self.view.addSubview(draggableBackground)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
